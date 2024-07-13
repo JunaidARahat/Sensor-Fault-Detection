269f35d45e87eb11f50306f47a34ef2e11683d64
JunaidARahat <mjunaidrahat1990@gmail.com>
2024-07-13 18:52:36 +0500

    data ingestion added successfully

---
+++ app.py
@@ -0,0 +1,53 @@
+from sensor.configuration.mongodb_connection import MongoDBClient
+from sensor.exception import SensorException
+import os , sys
+from sensor.logger import logging
+#from  sensor.utils import dump_csv_file_to_mongodb_collecton
+#from sensor.entity.config_entity  import TrainingPipelineConfig,DataIngestionConfig
+
+from sensor.pipline.training_pipeline import TrainPipeline
+
+# def test_exception():
+#     try:
+#         logging.info("ki yaha p bhaiaa ek error ayegi diveision by zero wali error ")
+#         a=1/0
+#     except Exception as e:
+#        raise SensorException(e,sys) 
+
+
+
+if __name__ == "__main__":
+
+    # file_path="/Users/myhome/Downloads/sensorlive/aps_failure_training_set1.csv"
+    # database_name="ineuron"
+    # collection_name ="sensor"
+    # dump_csv_file_to_mongodb_collection(file_path,database_name,collection_name)
+
+    training_pipeline = TrainPipeline()
+    training_pipeline.run_pipeline()
+
+
+
+
+
+
+
+
+
+  
+
+
+
+
+
+
+
+
+
+
+
+
+    # try:
+    #     test_exception()
+    # except Exception as e:
+    #     print(e)
---
+++ sensor/constants/database.py
@@ -0,0 +1,2 @@
+DATABASE_NAME = "sensor"
+COLLECTION_NAME = "apsdata"
