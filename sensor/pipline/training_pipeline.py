e89827fe9a234b7843b2e7fc7b3662fdbb8a33fe
JunaidARahat <mjunaidrahat1990@gmail.com>
2024-07-16 22:24:46 +0500

    data validation added

--- sensor/pipline/training_pipeline.py
+++ sensor/pipline/training_pipeline.py
@@ -1,9 +1,10 @@
-from sensor.entity.config_entity import TrainingPipelineConfig,DataIngestionConfig
-from sensor.entity.artifact_entity import DataIngestionArtifact
+from sensor.entity.config_entity import TrainingPipelineConfig,DataIngestionConfig,DataValidationConfig
+from sensor.entity.artifact_entity import DataIngestionArtifact,DataValidationArtifact
 from sensor.exception import SensorException
 import sys,os
 from sensor.logger import logging
 from sensor.components.data_ingestion import DataIngestion
+from sensor.components.data_validation import DataValidation
 
 class TrainPipeline:
     def __init__(self):
@@ -21,7 +22,22 @@ class TrainPipeline:
             return data_ingestion_artifact
         except  Exception as e:
             raise  SensorException(e,sys)
-        
+    
+
+
+    def start_data_validaton(self,data_ingestion_artifact:DataIngestionArtifact)->DataValidationArtifact:
+        try:
+            data_validation_config = DataValidationConfig(training_pipeline_config=self.training_pipeline_config)
+            data_validation = DataValidation(data_ingestion_artifact=data_ingestion_artifact,
+            data_validation_config = data_validation_config
+            )
+            data_validation_artifact = data_validation.initiate_data_validation()
+            return data_validation_artifact
+        except  Exception as e:
+            raise  SensorException(e,sys)
+    
+
+
 
 
     
@@ -39,6 +55,8 @@ class TrainPipeline:
             
 
             data_ingestion_artifact:DataIngestionArtifact = self.start_data_ingestion()
+            data_validation_artifact=self.start_data_validaton(data_ingestion_artifact=data_ingestion_artifact)
+
         
         except  Exception as e:
             raise  SensorException(e,sys)
