b63db6ed283617d73cd32ad48f57f0db5dddb09f
JunaidARahat <mjunaidrahat1990@gmail.com>
2024-07-17 12:30:08 +0500

    model evaluation added

--- sensor/constants/training_pipeline/__init__.py
+++ sensor/constants/training_pipeline/__init__.py
@@ -52,3 +52,11 @@ MODEL_TRAINER_TRAINED_MODEL_DIR: str = "trained_model"
 MODEL_TRAINER_TRAINED_MODEL_NAME: str = "model.pkl"
 MODEL_TRAINER_EXPECTED_SCORE: float = 0.6
 MODEL_TRAINER_OVER_FIITING_UNDER_FITTING_THRESHOLD: float = 0.05
+
+
+"""
+Model Evaluation ralated constant start with Model Evaluation VAR NAME
+"""
+MODEL_EVALUATION_DIR_NAME: str = "model_evaluation"
+MODEL_EVALUATION_CHANGED_THRESHOLD_SCORE: float = 0.02
+MODEL_EVALUATION_REPORT_NAME= "report.yaml"
--- sensor/pipline/training_pipeline.py
+++ sensor/pipline/training_pipeline.py
@@ -1,9 +1,10 @@
 from sensor.entity.config_entity import (TrainingPipelineConfig,DataIngestionConfig,
                                          DataValidationConfig,DataTransformationConfig,
-                                         ModelTrainerConfig)
+                                         ModelTrainerConfig,ModelEvaluationConfig)
 from sensor.entity.artifact_entity import (DataIngestionArtifact,
                                         DataValidationArtifact,DataTransformationArtifact,
-                                        DataTransformationArtifact)
+                                        DataTransformationArtifact,
+                                        ModelTrainerArtifact)
 from sensor.exception import SensorException
 import sys,os
 from sensor.logger import logging
@@ -11,6 +12,7 @@ from sensor.components.data_ingestion import DataIngestion
 from sensor.components.data_validation import DataValidation
 from sensor.components.data_transformation import DataTransformation
 from sensor.components.model_trainer import ModelTrainer
+from sensor.components.model_evaluation import ModelEvaluation
 class TrainPipeline:
     def __init__(self):
         self.training_pipeline_config = TrainingPipelineConfig()
@@ -65,6 +67,24 @@ class TrainPipeline:
             return model_trainer_artifact
         except  Exception as e:
             raise  SensorException(e,sys)
+    
+
+    def start_model_evaluation(self,data_validation_artifact:DataValidationArtifact,
+                                 model_trainer_artifact:ModelTrainerArtifact,
+                                ):
+        
+        try:
+            model_eval_config = ModelEvaluationConfig(self.training_pipeline_config)
+
+            model_eval = ModelEvaluation(model_eval_config, data_validation_artifact, model_trainer_artifact)
+
+            model_eval_artifact = model_eval.initiate_model_evaluation()
+            return model_eval_artifact
+        
+        except  Exception as e:
+            raise  SensorException(e,sys)
+        
+
 
 
 
@@ -82,6 +102,10 @@ class TrainPipeline:
             data_validation_artifact=self.start_data_validaton(data_ingestion_artifact=data_ingestion_artifact)
             data_transformation_artifact = self.start_data_transformation(data_validation_artifact=data_validation_artifact)
             model_trainer_artifact = self.start_model_trainer(data_transformation_artifact)
+            model_eval_artifact = self.start_model_evaluation(data_validation_artifact, model_trainer_artifact) 
+            
+            if not model_eval_artifact.is_model_accepted:
+                raise Exception("Trained model is not better than the best model")
 
         except  Exception as e:
             raise  SensorException(e,sys)
